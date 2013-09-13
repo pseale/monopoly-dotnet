e6fea86b15e3254bb3ebbaaf5f109d35fcd26b88
Peter Seale <peter@pseale.com>
2013-09-12 19:55:11 -0500

    move dice rolls to a proper CQRS Command

---
+++ src/MonopolyDotNet/MonopolyWeb/Models/Commands/RollDiceCommand.cs
@@ -0,0 +1,12 @@
+﻿using MonopolyWeb.Models.Core;
+
+namespace MonopolyWeb.Models.Commands
+{
+  public class RollDiceCommand
+  {
+    public static void Execute(Game game)
+    {
+      game.Roll();
+    }
+  }
+}

22d42e3971ddac32bd1e99552b474afa2f9e98ae
Peter Seale <peter@pseale.com>
2013-09-17 13:07:19 -0500

    add computer behavior for passing go (collecting )

--- src/MonopolyDotNet/MonopolyTests/Tests/PassingGoTests/OpponentArrivingAtGo.cs
+++ src/MonopolyDotNet/MonopolyTests/Tests/PassingGoTests/OpponentArrivingAtGo.cs
@@ -1,7 +1,24 @@
-﻿namespace MonopolyTests.Tests.PassingGoTests
+﻿using MonopolyTests.Builders;
+using NUnit.Framework;
+
+namespace MonopolyTests.Tests.PassingGoTests
 {
-  public class OpponentArrivingAtGo
+  [TestFixture]
+  public class OpponentArrivingAtGo : WebTestBase
   {
-     
+    [Test]
+    public void When_opponent_arrives_at_GO__do_NOT_collect_200_dollars()
+    {
+      TestHelper.StartAGame();
+      TestHelper.WithOpponent1Roll(10, () =>
+      {
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll"); //opponent 1 arrives at GO
+
+        Assert.IsTrue(browser.Opponent1HasMoney(1500));
+      });
+    }
   }
 }
--- src/MonopolyDotNet/MonopolyTests/Tests/PassingGoTests/OpponentPassingGo.cs
+++ src/MonopolyDotNet/MonopolyTests/Tests/PassingGoTests/OpponentPassingGo.cs
@@ -1,7 +1,25 @@
-﻿namespace MonopolyTests.Tests.PassingGoTests
+﻿using MonopolyTests.Builders;
+using NUnit.Framework;
+
+namespace MonopolyTests.Tests.PassingGoTests
 {
-  public class OpponentPassingGo
+  [TestFixture]
+  public class OpponentPassingGo : WebTestBase
   {
-     
+    [Test]
+    public void When_opponent_passes_GO__collect_200_dollars()
+    {
+      TestHelper.StartAGame();
+      TestHelper.WithOpponent1Roll(10, () =>
+      {
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll");
+        browser.ClickButton("Roll"); //arrives at GO
+        browser.ClickButton("Roll"); //passes GO
+
+        Assert.IsTrue(browser.Opponent1HasMoney(1700));
+      });
+    }
   }
 }

0ffc3244c9340fffc7ae326e0c8d20cd1ccc1597
Peter Seale <peter@pseale.com>
2013-09-21 20:05:27 -0500

    refactor: remove duplication on NewGame form

---
+++ src/MonopolyDotNet/MonopolyWeb/Models/ViewModels/OpponentsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MonopolyWeb.Models.ViewModels
+{
+  public class OpponentsViewModel
+  {
+    public static string[] All;
+
+    static OpponentsViewModel()
+    {
+      All = new string[] { "Rube", "Chester", "Mike Shishesky", "Mike Krzyzewski", "Adolf" };
+    }
+  }
+}
